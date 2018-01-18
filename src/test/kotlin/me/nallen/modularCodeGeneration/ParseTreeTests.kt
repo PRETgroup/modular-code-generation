10310487d9d11f706b840f9065b36077072267bf
Nathan Allen <nathan.allen.nz@gmail.com>
2018-01-18 16:57:29 +1300

    Updated some test cases

--- src/test/kotlin/me/nallen/modularCodeGeneration/ParseTreeTests.kt
+++ src/test/kotlin/me/nallen/modularCodeGeneration/ParseTreeTests.kt
@@ -21,15 +21,23 @@ class ParseTreeTests : StringSpec() {
                 EquationSet("4 * exp(6 + 2) - 7", "(4 * exp(6 + 2)) - 7", 11916.8319),
                 EquationSet("6 + -7 * -4", "6 + ((-7) * (-4))", 34.0),
                 EquationSet("9 + sqrt(7.234)", "9 + sqrt(7.234)", 11.6896),
-                EquationSet("a == 7 || b != 5", "(a == 7) || (b != 5)"),
-                EquationSet("a <= 3 && b >= 2 || c < 2 || d > 7", "(((a <= 3) && (b >= 2)) || (c < 2)) || (d > 7)"),
-                EquationSet("!b == 5", "(!b) == 5"),
-                EquationSet("!(b == 5)", "!(b == 5)"),
-                EquationSet("2 + my_custom_function(a, b, test() )", "2 + my_custom_function(a, b, test() )"),
+                EquationSet("6 == 7 || 9 != 5", "(6 == 7) || (9 != 5)", true),
+                EquationSet("8 != 6 + true", "8 != (6 + true)", true),
+                EquationSet("(2.5 <= 3 && 2.5 >= 2 || 4 < 2) && 5 - false", "(((2.5 <= 3) && (2.5 >= 2)) || (4 < 2)) && (5 - false)", true),
+                EquationSet("!1 == 5", "(!1) == 5", false),
+                EquationSet("!(1 == 5)", "!(1 == 5)", true),
+                EquationSet("false || 5 < 7", "(false) || (5 < 7)", true),
+                EquationSet("true && 5 > 7", "(true) && (5 > 7)", false),
+                EquationSet("(5 > 7) || (5 - 5)", "(5 > 7) || (5 - 5)", false),
+                EquationSet("(5 < 7) + my_custom_function(a, b, test() )", "(5 < 7) + my_custom_function(a, b, test() )"),
+                EquationSet("2 + b", "2 + b"),
+                EquationSet("a || false", "a || false"),
+                EquationSet("true && test()", "true && test()"),
 
                 EquationSet("my_custom_function(a, b c)"),
                 EquationSet("my_custom_function(a, b, c"),
                 EquationSet("my_custom_function(a, b, c -)"),
+                EquationSet("my_custom_function(a, , c)"),
                 EquationSet("broken_function_52<3>"),
                 EquationSet("5 * / 2"),
                 EquationSet("4 3 - + 2"),
@@ -42,7 +50,7 @@ class ParseTreeTests : StringSpec() {
                     ParseTreeItem.generate(equation.original) shouldBe ParseTreeItem.generate(equation.parenthesised)
                 }
             }
-            else {
+            else if(equation.result == null) {
                 ("Failure of " + equation.original) {
                     shouldThrow<IllegalArgumentException> {
                         ParseTreeItem.generate(equation.original)
@@ -59,6 +67,13 @@ class ParseTreeTests : StringSpec() {
                     }
                     else {
                         result shouldBe equation.result
+                    }
+                }
+            }
+            else if(equation.parenthesised != null) {
+                "Should not evaluate ${equation.original}" {
+                    shouldThrow<IllegalArgumentException> {
+                        ParseTreeItem.generate(equation.original).evaluate()
                     }
                 }
             }
