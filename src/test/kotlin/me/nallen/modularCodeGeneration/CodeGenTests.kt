761cf0debb71129c908b8ae0f422e5dbc9d60961
Nathan Allen <nathan.allen.nz@gmail.com>
2018-01-18 17:06:17 +1300

    Tests for more of the config options for codegen

--- src/test/kotlin/me/nallen/modularCodeGeneration/CodeGenTests.kt
+++ src/test/kotlin/me/nallen/modularCodeGeneration/CodeGenTests.kt
@@ -1,8 +1,7 @@
 package me.nallen.modularCodeGeneration
 
 import io.kotlintest.specs.StringSpec
-import me.nallen.modularCodeGeneration.codeGen.CodeGenLanguage
-import me.nallen.modularCodeGeneration.codeGen.CodeGenManager
+import me.nallen.modularCodeGeneration.codeGen.*
 import me.nallen.modularCodeGeneration.description.Importer
 import java.io.File
 
@@ -18,8 +17,15 @@ class CodeGenTests : StringSpec() {
                         val imported = Importer.import(main.absolutePath)
 
                         val network = imported.first
-                        val config = imported.second
+                        var config = imported.second
 
+                        config = config.copy(parametrisationMethod = ParametrisationMethod.COMPILE_TIME)
+                        CodeGenManager.generateForNetwork(network, CodeGenLanguage.C, "build/tmp/codegen", config)
+
+                        config = config.copy(parametrisationMethod = ParametrisationMethod.RUN_TIME, logging = Logging())
+                        CodeGenManager.generateForNetwork(network, CodeGenLanguage.C, "build/tmp/codegen", config)
+
+                        config = config.copy(maximumInterTransitions = 4, requireOneIntraTransitionPerTick = true)
                         CodeGenManager.generateForNetwork(network, CodeGenLanguage.C, "build/tmp/codegen", config)
                     }
                 }
