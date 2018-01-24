2e624f8f7f0ed45b21f278f177213b91b5b95448
Nathan Allen <nathan.allen.nz@gmail.com>
2018-01-24 16:57:09 +1300

    Added in testing that the C files compile

--- src/test/kotlin/me/nallen/modularCodeGeneration/CodeGenTests.kt
+++ src/test/kotlin/me/nallen/modularCodeGeneration/CodeGenTests.kt
@@ -1,19 +1,25 @@
 package me.nallen.modularCodeGeneration
 
+import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.StringSpec
 import me.nallen.modularCodeGeneration.codeGen.*
 import me.nallen.modularCodeGeneration.description.Importer
 import java.io.File
+import java.io.IOException
+import java.util.concurrent.TimeUnit
 
 class CodeGenTests : StringSpec() {
     init {
+        val canMake = "make".runCommand(File("build"), ProcessBuilder.Redirect.PIPE) != 127
+
         File("examples").list().forEach {
             val folder = File("examples", it)
             if(folder.isDirectory) {
                 val main = File(folder, "main.yaml")
 
                 if(main.exists() && main.isFile) {
-                    ("Can Generate Code For " + it) {
+
+                    ("Can Generate" + if(canMake) { " and Compile" } else { "" } + " Code For  $it") {
                         val imported = Importer.import(main.absolutePath)
 
                         val network = imported.first
@@ -21,18 +27,53 @@ class CodeGenTests : StringSpec() {
 
                         config = config.copy(parametrisationMethod = ParametrisationMethod.COMPILE_TIME)
                         CodeGenManager.generateForNetwork(network, CodeGenLanguage.C, "build/tmp/codegen", config)
+                        if(canMake) {
+                            "make".runCommand(File("build/tmp/codegen")) shouldBe 0
+                        }
 
                         config = config.copy(indentSize = -1, parametrisationMethod = ParametrisationMethod.RUN_TIME, logging = Logging())
                         CodeGenManager.generateForNetwork(network, CodeGenLanguage.C, "build/tmp/codegen", config)
+                        if(canMake) {
+                            "make".runCommand(File("build/tmp/codegen")) shouldBe 0
+                        }
 
                         config = config.copy(maximumInterTransitions = 4, requireOneIntraTransitionPerTick = true)
                         CodeGenManager.generateForNetwork(network, CodeGenLanguage.C, "build/tmp/codegen", config)
+                        if(canMake) {
+                            "make".runCommand(File("build/tmp/codegen")) shouldBe 0
+                        }
 
                         config = config.copy(maximumInterTransitions = 4, requireOneIntraTransitionPerTick = false)
                         CodeGenManager.generateForNetwork(network, CodeGenLanguage.C, "build/tmp/codegen", config)
+                        if(canMake) {
+                            "make".runCommand(File("build/tmp/codegen")) shouldBe 0
+                        }
+                    }
+
+                    if(!canMake) {
+                        "Can Compile Code For $it" {
+
+                        }.config(enabled = false)
                     }
                 }
             }
+        }
+    }
+
+    private fun String.runCommand(workingDir: File, redirect: ProcessBuilder.Redirect = ProcessBuilder.Redirect.INHERIT): Int {
+        try {
+            val parts = this.split("\\s".toRegex())
+            val proc = ProcessBuilder(*parts.toTypedArray())
+                    .directory(workingDir)
+                    .redirectOutput(redirect)
+                    .redirectError(redirect)
+                    .start()
+
+            proc.waitFor(60, TimeUnit.MINUTES)
+            return proc.exitValue()
+        }
+        catch(ex: IOException) {
+            return 127;
         }
     }
 
