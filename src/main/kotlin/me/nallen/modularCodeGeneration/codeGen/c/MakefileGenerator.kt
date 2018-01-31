7fb31b6926b3c58b970c0eb0d5d04c8e23f8f9e6
Nathan Allen <nathan.allen.nz@gmail.com>
2018-01-31 16:36:33 +1300

    Makefile and Utils generation commenting

--- src/main/kotlin/me/nallen/modularCodeGeneration/codeGen/c/MakefileGenerator.kt
+++ src/main/kotlin/me/nallen/modularCodeGeneration/codeGen/c/MakefileGenerator.kt
@@ -4,75 +4,114 @@ import me.nallen.modularCodeGeneration.codeGen.Configuration
 import me.nallen.modularCodeGeneration.codeGen.ParametrisationMethod
 import me.nallen.modularCodeGeneration.hybridAutomata.AutomataInstance
 
+/**
+ * The class that contains methods to do with the generation of the MakeFile for the network
+ */
 object MakefileGenerator {
     private var instances: Map<String, AutomataInstance> = LinkedHashMap()
     private var config: Configuration = Configuration()
 
+    /**
+     * Generates a string that represents the Makefile for the network. The final generated program will be named by the
+     * provided networkName.
+     */
     fun generate(networkName: String, instances: Map<String, AutomataInstance>, config: Configuration = Configuration()): String {
         this.instances = instances
         this.config = config
 
         val result = StringBuilder()
 
+        // The final target is set by the networkName
         result.appendln("TARGET = $networkName")
+
+        // Default compiler settings, using gcc
         result.appendln("CC = gcc")
         result.appendln("CFLAGS = -c -O2 -Wall")
         result.appendln("LDFLAGS = -g -Wall")
         result.appendln("LDLIBS = -lm")
         result.appendln()
 
+        // The default build target is to build the executable
         result.appendln("build: $(TARGET)")
         result.appendln()
 
+        // We keep track of the sources for when we link at the end
         val sources = ArrayList<String>()
 
+        // We can only generate code if there are any instances
         if(instances.isNotEmpty()) {
+            // Depending on the parametrisation method, we'll do things slightly differently
             if(config.parametrisationMethod == ParametrisationMethod.COMPILE_TIME) {
+                // Compile time parametrisation means compiling each instance
                 for((name, instance) in instances) {
+                    // Generate the file name that we'll be looking for
                     val deliminatedName = Utils.createFileName(name)
 
+                    // Generated the folder name that we'll be looking for
                     val subfolder = if(instance.automata.equals(networkName, true)) { instance.automata + " Files" } else { instance.automata }
                     val deliminatedFolder = Utils.createFolderName(subfolder)
+
+                    // Create the compile command for the file
                     result.append(generateCompileCommand(deliminatedName, listOf("$deliminatedFolder/$deliminatedName.c"), listOf("$deliminatedFolder/$deliminatedName.h", CCodeGenerator.CONFIG_FILE)))
                     result.appendln()
+
+                    // Keep track of the sources
                     sources.add("Objects/$deliminatedName")
                 }
             }
             else {
+                // We only want to generate each definition once, so keep a track of them
                 val generated = ArrayList<String>()
                 for((_, instance) in instances) {
                     if (!generated.contains(instance.automata)) {
                         generated.add(instance.automata)
 
+                        // Generate the file name that we'll be looking for
                         val deliminatedName = Utils.createFileName(instance.automata)
+
+                        // Create the compile command for the file
                         result.append(generateCompileCommand(deliminatedName, listOf("$deliminatedName.c"), listOf("$deliminatedName.h", CCodeGenerator.CONFIG_FILE)))
                         result.appendln()
+
+                        // Keep track of the sources
                         sources.add("Objects/$deliminatedName")
                     }
                 }
             }
         }
 
+        // Create the compile command for the runnable main file
         result.append(generateCompileCommand("runnable", listOf("runnable.c"), listOf(CCodeGenerator.CONFIG_FILE)))
         result.appendln()
+
+        // Keep track of the sources
         sources.add("Objects/runnable")
 
+        // Generate the link command, with all the sources
         result.append(generateLinkCommand("$(TARGET)", sources))
         result.appendln()
 
+        // Now we have the clean command, which deletes the target and all objects
         result.appendln(".PHONY: clean")
         result.appendln("clean:")
         result.appendln("\t@echo Removing compiled binaries...")
         result.appendln("\t@rm -rf $(TARGET) Objects/* *~")
         result.appendln()
 
+        // Return the final Makefile
         return result.toString().trim()
     }
 
+    /**
+     * Generates a compile command for the given object, taking in the given sources and dependencies for use in the
+     * command. Note that sources are automatically by default dependencies
+     */
     private fun generateCompileCommand(name: String, sources: List<String>, dependencies: List<String>): String {
         val result = StringBuilder()
 
+        // The output will be in the Objects directory
         result.append("Objects/$name:")
+        // And we need to list all the dependencies we have (sources are also dependencies)
         for(source in sources) {
             result.append(" $source")
         }
@@ -80,32 +119,48 @@ object MakefileGenerator {
             result.append(" $dependency")
         }
         result.appendln()
+
+        // Let the user know what it's currently compiling
         result.appendln("\t@echo Building $name...")
+        // And then compile (making the directory if needed)
         result.append("\t@mkdir -p Objects; $(CC) $(CFLAGS)")
         if(sources.size == 1) {
+            // If there's only one source then we use the shorthand for Makefile sources
             result.append(" $<")
         }
         else {
+            // Otherwise let's just list them all
             for (source in sources) {
                 result.append(" $source")
             }
         }
+        // The output also uses the Makefile shorthand
         result.appendln(" -o $@")
 
+        // Return the compile command
         return result.toString()
     }
 
+    /**
+     * Generates a linker command for the program, taking in the given sources
+     */
     private fun generateLinkCommand(output: String, sources: List<String>): String {
         val result = StringBuilder()
 
+        // Output file is the name of this task
         result.append("$output:")
+        // It depends on all of the sources
         for(source in sources) {
             result.append(" $source")
         }
         result.appendln()
+
+        // Let the user know what it's currently linking
         result.appendln("\t@echo Building $output...")
+        // And then link, using the Makefile shorthands for source and output files
         result.append("\t$(CC) $(LDFLAGS) $^ $(LDLIBS) -o $@")
 
+        // Return the linker command
         return result.toString()
     }
 }
--- src/main/kotlin/me/nallen/modularCodeGeneration/codeGen/c/Utils.kt
+++ src/main/kotlin/me/nallen/modularCodeGeneration/codeGen/c/Utils.kt
@@ -10,10 +10,19 @@ import me.nallen.modularCodeGeneration.utils.convertWordDelimiterConvention
 
 typealias ParseTreeLocality = me.nallen.modularCodeGeneration.parseTree.Locality
 
+/**
+ * A set of utilities for C Code generation regarding types, ParseTree generation, and naming conventions
+ */
 object Utils {
+    // If something uses STEP_SIZE, we don't want to throw an error when there's no default value for it, because it's
+    // defined in another file
     val DEFAULT_CUSTOM_VARIABLES = mapOf("STEP_SIZE" to "STEP_SIZE")
 
+    /**
+     * Convert our VariableType to a C type
+     */
     fun generateCType(type: VariableType?): String {
+        // Switch on the type, and return the appropriate C type
         return when(type) {
             null -> "void"
             VariableType.BOOLEAN -> "bool"
@@ -21,21 +30,35 @@ object Utils {
         }
     }
 
+    /**
+     * Convert our VariableType to the printf specifier used for logging a variable of that type
+     */
     fun generatePrintfType(type: VariableType): String {
+        // Switch on the type, and return the appropriate printf specifier
         return when(type) {
             VariableType.BOOLEAN -> "%d"
             VariableType.REAL -> "%f"
         }
     }
 
+    /**
+     * Generates a string that does some arbitrary function on all variables of a given locality inside an automata.
+     * The arbitrary function must take in a single Variable, and return a String. In addition, a comment can be added
+     * at the start of the block of variables
+     */
     fun performVariableFunctionForLocality(automata: HybridAutomata, locality: Locality, function: (v: Variable) -> String, config: Configuration = Configuration(), comment: String? = null, depth: Int = 1): String {
         val result = StringBuilder()
 
+        // We only need to do anything if there are any variables of the requested type
         if(automata.variables.any{it.locality == locality}) {
             result.appendln()
+            // If we need to generate a comment
             if(comment != null)
+                // Generate the comment, at the correct indent, with the locality named after
                 result.appendln("${config.getIndent(depth)}// $comment ${locality.getTextualName()}")
 
+            // Now for each variable of the right locality, we want to add the output of the arbitrary function with the
+            // variable as input
             automata.variables
                     .filter{it.locality == locality}
                     .sortedBy { it.type }
@@ -44,49 +67,93 @@ object Utils {
                     .forEach { result.appendln("${config.getIndent(depth)}$it") }
         }
 
+        // And now return the overall result
         return result.toString()
     }
 
+    /**
+     * Generates a Folder Name from the given string(s) that conforms to C style guidelines
+     */
     fun createFolderName(vararg original: String): String {
+        // Folder Names use UpperCamelCase
         return original.convertWordDelimiterConvention(NamingConvention.UPPER_CAMEL_CASE)
     }
 
+    /**
+     * Generates a File Name from the given string(s) that conforms to C style guidelines
+     */
     fun createFileName(vararg original: String): String {
+        // File Names use snake_case
         return original.convertWordDelimiterConvention(NamingConvention.SNAKE_CASE)
     }
 
+    /**
+     * Generates a Type Name from the given string(s) that conforms to C style guidelines
+     */
     fun createTypeName(vararg original: String): String {
+        // Type Names use UpperCamelCase
         return original.convertWordDelimiterConvention(NamingConvention.UPPER_CAMEL_CASE)
     }
 
+    /**
+     * Generates a Variable Name from the given string(s) that conforms to C style guidelines
+     */
     fun createVariableName(vararg original: String): String {
+        // Variable Names use snake_case
         return original.convertWordDelimiterConvention(NamingConvention.SNAKE_CASE)
     }
 
+    /**
+     * Generates a Function Name from the given string(s) that conforms to C style guidelines
+     */
     fun createFunctionName(vararg original: String): String {
+        // Function Names use UpperCamelCase
         return original.convertWordDelimiterConvention(NamingConvention.UPPER_CAMEL_CASE)
     }
 
+    /**
+     * Generates a Macro Name from the given string(s) that conforms to C style guidelines
+     */
     fun createMacroName(vararg original: String): String {
+        // Macro Names use UPPER_SNAKE_CASE
         return original.convertWordDelimiterConvention(NamingConvention.UPPER_SNAKE_CASE)
     }
 
+    /**
+     * Looks at a provided ParseTreeItem and its child and generates a C representation of the formula
+     * If required, this method will add brackets around any operations that require them
+     */
     private fun padOperand(item: ParseTreeItem, operand: ParseTreeItem, prefixData: PrefixData): String {
+        // We need to look at the precedence of both this operator and the child to decide whether brackets are needed
         var precedence = item.getPrecedence()
+
+        // An interesting case happens when one of the operators is not commutative and the child has 2 or more arguments
+        // In this case we try to be safe by including brackets if they previously had the same precedence
         if((!operand.getCommutative() || !item.getCommutative()) && operand.getChildren().size > 1) {
             precedence--
         }
+
+        // If the current operation's precedence is more than the childs, then we want brackets
         if(precedence < operand.getPrecedence())
             return "(" + generateCodeForParseTreeItem(operand, prefixData) + ")"
 
-        // Special cases
+        // Special cases for C revolve around Or and And
         if(item is Or && operand is And)
             return "(" + generateCodeForParseTreeItem(operand, prefixData) + ")"
 
+        // Otherwise no brackets
         return generateCodeForParseTreeItem(operand, prefixData, item)
     }
 
+    /**
+     * Generates a C representation of the given ParseTreeItem. PrefixData is used for determining any text that should
+     * appear before any variable uses
+     *
+     * This will recursively go through the Parse Tree to generate the string, including adding brackets where necessary
+     */
     fun generateCodeForParseTreeItem(item: ParseTreeItem, prefixData: PrefixData = PrefixData(""), parent: ParseTreeItem? = null): String {
+        // For each possible ParseTreeItem we have a different output string that will be generated
+        // We recursively call these generation functions until we reach the end of the tree
         return when (item) {
             is And -> padOperand(item, item.operandA, prefixData) + " && " + padOperand(item, item.operandB, prefixData)
             is Or -> padOperand(item, item.operandA, prefixData) + " || " + padOperand(item, item.operandB, prefixData)
@@ -98,11 +165,21 @@ object Utils {
             is Equal -> padOperand(item, item.operandA, prefixData) + " == " + padOperand(item, item.operandB, prefixData)
             is NotEqual -> padOperand(item, item.operandA, prefixData) + " != " + padOperand(item, item.operandB, prefixData)
             is FunctionCall -> {
+                // Functions have a lot of extra logic we need to do
+                // Firstly we should check if it's a delayed function call, which will have special handling
                 if(item.functionName == "delayed") {
+                    // If the name matches, and it has the correct number of arguments
                     if(item.arguments.size == 2) {
+                        // The first argument also should be a Variable
                         if(item.arguments[0] is me.nallen.modularCodeGeneration.parseTree.Variable) {
+                            // Get the variable name we want to delay
                             val varName = item.arguments[0].getString()
+
+                            // If we are able to delay this variable
                             if(prefixData.delayedVariableTypes.containsKey(varName)) {
+                                // Generate the Delayable call for this variable. It requires the type of the variable,
+                                // the name of the variable (to point to the struct), and the time that we want to fetch
+                                // from
                                 return "${createFunctionName("Delayable", generateCType(prefixData.delayedVariableTypes[varName]), "Get")}(" +
                                         "&${generateCodeForParseTreeItem(item.arguments[0], prefixData)}_delayed, " +
                                         "${generateCodeForParseTreeItem(item.arguments[1], prefixData)})"
@@ -111,34 +188,51 @@ object Utils {
                     }
                 }
 
+                // Otherwise, let's build a function
                 val builder = StringBuilder()
 
+                // If we're in run time parametrisation, we pass the automata struct into the function for parameters
                 if(prefixData.requireSelfReferenceInFunctionCalls)
                     builder.append("me")
 
+                // Now add each argument to the function
                 for(argument in item.arguments) {
+                    // If needed, deliminate by a comma
                     if(builder.isNotEmpty()) builder.append(", ")
                     builder.append(generateCodeForParseTreeItem(argument, prefixData))
                 }
 
+                // And then return the final function name
                 return "${Utils.createFunctionName(item.functionName)}($builder)"
             }
             is Literal -> item.value
             is me.nallen.modularCodeGeneration.parseTree.Variable -> {
+                // Variables also have a bit of extra logic that's needed
+
+                // If the variable has a value assigned to it
                 if(item.value != null)
+                    // Then we want to just replace this with the string representing the value
                     return padOperand(parent ?: item, item.value!!, prefixData)
                 else {
+                    // Otherwise we want to generate this variable
+                    // It may consist of data inside structs, separated by periods
                     val parts = item.name.split(".")
                     val builder = StringBuilder()
 
+                    // For each part
                     for(part in parts) {
+                        // If needed, deliminate by a period
                         if(builder.isNotEmpty()) builder.append(".")
+                        // If we have a pre-determined value for this variable
                         if(prefixData.customVariableNames.containsKey(part))
+                            // Then use that
                             builder.append(prefixData.customVariableNames[part]!!)
                         else
+                            // Otherwise generate the C name for this variable
                             builder.append("${prefixData.prefix}${Utils.createVariableName(part)}")
                     }
 
+                    // And return the final variable name
                     return builder.toString()
                 }
             }
@@ -152,17 +246,28 @@ object Utils {
         }
     }
 
+    /**
+     * Generates a C representation of the given Program. PrefixData is used for determining any text that should appear
+     * before any variable uses
+     *
+     * This will recursively go through the Program to generate the string, including adding brackets where necessary
+     */
     fun generateCodeForProgram(program: Program, config: Configuration, depth: Int = 0, prefixData: PrefixData = PrefixData("")): String {
         val builder = StringBuilder()
 
+        // First, we want to declare and initialise any internal variables that exist in this program
         program.variables.filter({it.locality == ParseTreeLocality.INTERNAL})
                 .filterNot { prefixData.customVariableNames.containsKey(it.name) }
                 .forEach { builder.appendln("${config.getIndent(depth)}${Utils.generateCType(it.type)} ${Utils.createVariableName(it.name)};") }
         if(builder.isNotEmpty())
             builder.appendln()
 
+        // Now, we need to go through each line
         program.lines
                 .map {
+                    // And convert the ProgramLine into a string representation
+                    // Note that some of these will recursively call this method, as they contain their own bodies
+                    // (such as If statements)
                     when(it) {
                         is Statement -> "${Utils.generateCodeForParseTreeItem(it.logic, prefixData)};"
                         is Assignment -> "${Utils.generateCodeForParseTreeItem(it.variableName, prefixData)} = ${Utils.generateCodeForParseTreeItem(it.variableValue, prefixData)};"
@@ -174,13 +279,25 @@ object Utils {
                 }
                 .forEach { builder.appendln(it.prependIndent(config.getIndent(depth))) }
 
+        // And return the total program
         return builder.toString().trimEnd()
     }
 
+    /**
+     * A class that contains various information about variables and functions used when generating code
+     */
     data class PrefixData(
+            // A prefix that should be placed before all variable names
             val prefix: String,
+
+            // Whether or not to require the first argument of a custom function call to be a self-reference to the
+            // object's struct
             val requireSelfReferenceInFunctionCalls: Boolean = false,
+
+            // A set of the variables that can be delayed, and their types
             val delayedVariableTypes: Map<String, VariableType> = HashMap(),
+
+            // A set of variables whose names should be something other than the style convention
             val customVariableNames: Map<String, String> = DEFAULT_CUSTOM_VARIABLES
     )
 }
