ce9287ed9477be159cd5a9e3098584ba87b316b9
Nathan Allen <nathan.allen.nz@gmail.com>
2018-01-31 15:04:39 +1300

    Commenting of 2/6 C codegen files

--- src/main/kotlin/me/nallen/modularCodeGeneration/codeGen/c/CCodeGenerator.kt
+++ src/main/kotlin/me/nallen/modularCodeGeneration/codeGen/c/CCodeGenerator.kt
@@ -9,6 +9,11 @@ import me.nallen.modularCodeGeneration.hybridAutomata.HybridNetwork
 import me.nallen.modularCodeGeneration.parseTree.VariableType
 import java.io.File
 
+/**
+ * The class that contains methods to do with the generation of C code for a Hybrid Network.
+ *
+ * All generated files and the generated code itself will follow proper C style conventions.
+ */
 class CCodeGenerator {
     companion object {
         const val RUNNABLE = "runnable.c"
@@ -16,77 +21,121 @@ class CCodeGenerator {
         const val CONFIG_FILE = "config.h"
         const val DELAYABLE_HEADER = "delayable.h"
 
+        /**
+         * Generate C code for the FSM that represents a Hybrid Automata. The code will be placed into the provided
+         * directory, overwriting any contents that may already exist.
+         */
         private fun generateFsm(automata: HybridAutomata, dir: String, config: Configuration = Configuration()) {
             val outputDir = File(dir)
 
+            // If the directory doesn't already exist, we want to create it
             if(!outputDir.exists())
                 outputDir.mkdirs()
 
+            // Generate the Header File
             File(outputDir, "${Utils.createFileName(automata.name)}.h").writeText(HFileGenerator.generate(automata, config))
+            // Generate the Source File
             File(outputDir, "${Utils.createFileName(automata.name)}.c").writeText(CFileGenerator.generate(automata, config))
         }
 
+        /**
+         * Generate C code for the overall runnable that represents a Hybrid Network. The code will be placed into the
+         * provided directory, overwriting any contents that may already exist.
+         */
         private fun generateRunnable(network: HybridNetwork, dir: String, config: Configuration = Configuration()) {
             val outputDir = File(dir)
 
+            // If the directory doesn't already exist, we want to create it
             if(!outputDir.exists())
                 outputDir.mkdirs()
 
+            // Generate the Runnable File
             File(outputDir, RUNNABLE).writeText(RunnableGenerator.generate(network, config))
         }
 
+        /**
+         * Generate a Makefile for the overall program that represents a Hybrid Network. The code will be placed into
+         * the provided directory, overwriting any contents that may already exist.
+         */
         private fun generateMakefile(name: String, instances: Map<String, AutomataInstance>, dir: String, config: Configuration = Configuration()) {
             val outputDir = File(dir)
 
+            // If the directory doesn't already exist, we want to create it
             if(!outputDir.exists())
                 outputDir.mkdirs()
 
+            // Generate the Makefile
             File(outputDir, MAKEFILE).writeText(MakefileGenerator.generate(name, instances, config))
         }
 
+        /**
+         * Generate a configuration file for the overall program. The code will be placed into the provided directory,
+         * overwriting any contents that may already exist.
+         */
         private fun generateConfigFile(dir: String, config: Configuration = Configuration()) {
             val outputDir = File(dir)
 
+            // If the directory doesn't already exist, we want to create it
             if(!outputDir.exists())
                 outputDir.mkdirs()
 
+            // Generate the content
             val content = StringBuilder()
 
+            // Represent most of the config properties as #defines in the C
+            // Execution Settings
             content.appendln("#define STEP_SIZE ${config.execution.stepSize}")
             content.appendln("#define SIMULATION_TIME ${config.execution.simulationTime}")
+
+            // Logging Settings
             content.appendln("#define ENABLE_LOGGING ${if(config.logging.enable) 1 else 0}")
             content.appendln("#define LOGGING_FILE \"${config.logging.file}\"")
             content.appendln("#define LOGGING_INTERVAL ${config.logging.interval ?: config.execution.stepSize}")
 
+            // And write the content
             File(outputDir, CONFIG_FILE).writeText(content.toString())
         }
 
+        /**
+         * Generate Delayable files for the requested types. These files are used to allow for signals to be delayed and
+         * define both the struct that holds all the data, and the methods for accessing data. The code will be placed
+         * into the provided directory, overwriting any contents that may already exist.
+         */
         private fun generateDelayableFiles(types: List<VariableType>, dir: String, config: Configuration = Configuration()) {
             val outputDir = File(dir)
 
+            // If the directory doesn't already exist, we want to create it
             if(!outputDir.exists())
                 outputDir.mkdirs()
 
+            // Generate the content
             val content = StringBuilder()
 
+            // Create the guard for the file
             content.appendln("#ifndef DELAYABLE_H_")
             content.appendln("#define DELAYABLE_H_")
             content.appendln()
 
+            // We depend on stdlib
             content.appendln("#include <stdlib.h>")
             content.appendln()
 
+            // If we're generating for a boolean type
             if(types.contains(VariableType.BOOLEAN)) {
+                // We need to create the type for bool, and true / false
                 content.appendln("typedef int bool;")
                 content.appendln("#define false 0")
                 content.appendln("#define true 1")
                 content.appendln()
             }
 
+            // Include the config file so we have access to step size
             content.appendln("#include \"$CONFIG_FILE\"")
             content.appendln()
 
+            // For each type we need to make delayable
             for(type in types) {
+                // Create the struct, which includes a buffer, max length, and current index
                 content.appendln("// Delayable struct for type ${Utils.generateCType(type)}")
                 content.appendln("typedef struct {")
                 content.appendln("${config.getIndent(1)}${Utils.generateCType(type)} *buffer;")
@@ -95,6 +144,8 @@ class CCodeGenerator {
                 content.appendln("} ${Utils.createTypeName("Delayable", Utils.generateCType(type))};")
                 content.appendln()
 
+                // Create the initialisation function, which instantiates the buffer, sets the max length, and resets
+                // the index to 0
                 content.appendln("// Initialisation function")
                 content.appendln("static inline void ${Utils.createFunctionName("Delayable", Utils.generateCType(type), "Init")}(${Utils.createTypeName("Delayable", Utils.generateCType(type))}* me, double max_delay) {")
                 content.appendln("${config.getIndent(1)}me->index = 0;")
@@ -103,6 +154,7 @@ class CCodeGenerator {
                 content.appendln("}")
                 content.appendln()
 
+                // Create the add function, which will add an item to the buffer and roll around the index if needed
                 content.appendln("// Add function")
                 content.appendln("static inline void ${Utils.createFunctionName("Delayable", Utils.generateCType(type), "Add")}(${Utils.createTypeName("Delayable", Utils.generateCType(type))}* me, ${Utils.generateCType(type)} value) {")
                 content.appendln("${config.getIndent(1)}me->index++;")
@@ -113,6 +165,8 @@ class CCodeGenerator {
                 content.appendln("}")
                 content.appendln()
 
+                // And finally create the get function, which will access the buffer at the correct position in the past
+                // If the requested delay is larger than the max delay then an error will occur
                 content.appendln("// Get function")
                 content.appendln("static inline ${Utils.generateCType(type)} ${Utils.createFunctionName("Delayable", Utils.generateCType(type), "Get")}(${Utils.createTypeName("Delayable", Utils.generateCType(type))}* me, double delay) {")
                 content.appendln("${config.getIndent(1)}int steps = (int) (delay / STEP_SIZE);")
@@ -127,41 +181,61 @@ class CCodeGenerator {
                 content.appendln()
             }
 
+            // Close the guard
             content.appendln("#endif // DELAYABLE_H_")
 
+            // And write the contents to disk
             File(outputDir, DELAYABLE_HEADER).writeText(content.toString())
         }
 
+        /**
+         * Generate all the files needed in the C Code generation of the given Hybrid Network. The code will be
+         * generated into the provided directory, overwriting any contents that may exist
+         */
         fun generateNetwork(network: HybridNetwork, dir: String, config: Configuration = Configuration()) {
             val outputDir = File(dir)
 
+            // If the directory doesn't already exist, we want to create it
             if(!outputDir.exists())
                 outputDir.mkdirs()
 
+            // We need to keep track of the variables we need to delay
             val delayedTypes = ArrayList<VariableType>()
-            // Generate FSM files
+
+            // Depending on the parametrisation method, we'll do things slightly differently
             if(config.parametrisationMethod == ParametrisationMethod.COMPILE_TIME) {
+                // Compile time parametrisation means creating a C file for each instance
                 for((name, instance) in network.instances) {
+                    // Create the parametrised copy of the automata
                     val automata = CodeGenManager.createParametrisedFsm(network, name, instance) ?: throw IllegalArgumentException("Unable to find base machine $name to instantiate!")
 
+                    // Add all the delayed types that we've found
                     delayedTypes.addAll(automata.variables.filter({it.canBeDelayed()}).map({it.type}))
 
+                    // We need to create a sub-folder for all the instances. We can run into issues if this is the same
+                    // name as the overall system, so check for that too
                     val subfolder = if(instance.automata.equals(network.name, true)) { instance.automata + " Files" } else { instance.automata }
 
+                    // Generate the code for the parametrised automata
                     generateFsm(automata, File(outputDir, Utils.createFolderName(subfolder)).absolutePath, config)
                 }
             }
             else  {
+                // We only want to generate each definition once, so keep a track of them
                 val generated = ArrayList<String>()
 
                 for((_, instance) in network.instances) {
+                    // Only generate if we haven't generated this definition before
                     if (!generated.contains(instance.automata)) {
                         generated.add(instance.automata)
 
+                        // Get the definition of the unparametrised automata
                         val automata = network.definitions.first({ it.name == instance.automata })
 
+                        // Add all the delayed types that we've found
                         delayedTypes.addAll(automata.variables.filter({it.canBeDelayed()}).map({it.type}))
 
+                        // Generate code for the unparametrised automata
                         generateFsm(automata, outputDir.absolutePath, config)
                     }
                 }
@@ -170,7 +244,7 @@ class CCodeGenerator {
             // Generate runnable
             generateRunnable(network, outputDir.absolutePath, config)
 
-            // Generate Makfile
+            // Generate Makefile
             generateMakefile(network.name, network.instances, outputDir.absolutePath, config)
 
             // Generate Config file
--- src/main/kotlin/me/nallen/modularCodeGeneration/codeGen/c/HFileGenerator.kt
+++ src/main/kotlin/me/nallen/modularCodeGeneration/codeGen/c/HFileGenerator.kt
@@ -6,121 +6,175 @@ import me.nallen.modularCodeGeneration.hybridAutomata.HybridAutomata
 import me.nallen.modularCodeGeneration.hybridAutomata.Locality
 import me.nallen.modularCodeGeneration.hybridAutomata.Variable
 
+/**
+ * The class that contains methods to do with the generation of Header Files for the Hybrid Automata
+ */
 object HFileGenerator {
     private var automata: HybridAutomata = HybridAutomata("Temp")
     private var config: Configuration = Configuration()
 
+    /**
+     * Generates a string that represents the Header File for the given Hybrid Automata
+     */
     fun generate(automata: HybridAutomata, config: Configuration = Configuration()): String {
         this.automata = automata
         this.config = config
 
+        // Let's start creating the header file!
         val result = StringBuilder()
 
+        // Generate the guard
         result.appendln("#ifndef ${Utils.createMacroName(automata.name)}_H_")
         result.appendln("#define ${Utils.createMacroName(automata.name)}_H_")
         result.appendln()
 
+        // Add in any includes we need
         result.appendln(generateIncludes())
 
+        // Create the enum for the states this automata uses
         result.appendln(generateEnum())
 
+        // Create the struct that holds all variables for the automata
         result.appendln(generateStruct())
 
+        // If we have run time parametrisation
         if(config.parametrisationMethod == ParametrisationMethod.RUN_TIME) {
+            // Then we need to declare a parametrisation function
             result.appendln("// ${automata.name} Default Parametrisation function")
             result.appendln("void ${Utils.createFunctionName(automata.name, "Parametrise")}(${Utils.createTypeName(automata.name)}* me);")
             result.appendln()
         }
 
+        // Declare the initialisation function
         result.appendln("// ${automata.name} Initialisation function")
         result.appendln("void ${Utils.createFunctionName(automata.name, "Init")}(${Utils.createTypeName(automata.name)}* me);")
         result.appendln()
 
+        // Declare the execution function
         result.appendln("// ${automata.name} Execution function")
         result.appendln("void ${Utils.createFunctionName(automata.name, "Run")}(${Utils.createTypeName(automata.name)}* me);")
         result.appendln()
 
+        // Close the guard
         result.appendln("#endif // ${Utils.createMacroName(automata.name)}_H_")
 
         return result.toString().trim()
     }
 
+    /**
+     * Generates a string that represents the list of includes that are needed for the automata
+     */
     private fun generateIncludes(): String {
         val result = StringBuilder()
 
+        // Default set of includes that are always needed
         result.appendln("#include <stdint.h>")
         result.appendln("#include <stdlib.h>")
         result.appendln("#include <stdio.h>")
         result.appendln("#include <math.h>") // This may not be needed in all cases
         // TODO: Check if FSM uses a math.h function (sqrt, pow, etc.)
 
+        // If we have delayed variables we need string.h
+        // TODO: Can this just be in delayable.h?
         if(automata.variables.any({it.canBeDelayed()}))
             result.appendln("#include <string.h>")
 
+        // Define the boolean type
         result.appendln()
         result.appendln("typedef int bool;")
         result.appendln("#define false 0")
         result.appendln("#define true 1")
 
         result.appendln()
+        // Include the config file, which will differ for depending on if we've done compile time parametrisation
         if(config.parametrisationMethod == ParametrisationMethod.COMPILE_TIME)
             result.appendln("#include \"../${CCodeGenerator.CONFIG_FILE}\"")
         else
             result.appendln("#include \"${CCodeGenerator.CONFIG_FILE}\"")
 
+        // If we have delayed variables
         if(automata.variables.any({it.canBeDelayed()})) {
+            // Include the delayable file, which will differ for depending on if we've done compile time parametrisation
             if(config.parametrisationMethod == ParametrisationMethod.COMPILE_TIME)
                 result.appendln("#include \"../${CCodeGenerator.DELAYABLE_HEADER}\"")
             else
                 result.appendln("#include \"${CCodeGenerator.DELAYABLE_HEADER}\"")
         }
 
+        // Return all the includes
         return result.toString()
     }
 
+    /**
+     * Generates a string that represents the enum of the possible locations that the automata can be in
+     */
     private fun generateEnum(): String {
         val result = StringBuilder()
 
+        // Declare the enum name (based on the automata name)
         result.appendln("// ${automata.name} States")
         result.appendln("enum ${Utils.createTypeName(automata.name, "States")} {")
+
+        // Then, for each location
         for((name) in automata.locations) {
+            // Create an entry in the num for it
             result.appendln("${config.getIndent(1)}${Utils.createMacroName(automata.name, name)},")
         }
         result.appendln("};")
 
+        // Return the enum
         return result.toString()
     }
 
+    /**
+     * Generates a string that represents the struct that holds all the data about the automata, including state and
+     * any variables
+     */
     private fun generateStruct(): String {
         val result = StringBuilder()
 
+        // Start the struct declaration
         result.appendln("// ${automata.name} Data Struct")
         result.appendln("typedef struct {")
 
+        // We need to store the state
         result.appendln("${config.getIndent(1)}// State")
         result.appendln("${config.getIndent(1)}enum ${Utils.createTypeName(automata.name, "States")} state;")
 
+        // We want to generate declarations for each of the different types of variables we might have
+        // Inputs
         result.append(Utils.performVariableFunctionForLocality(automata, Locality.EXTERNAL_INPUT, HFileGenerator::generateVariableDeclaration, config, "Declare"))
+        // Ouputs
         result.append(Utils.performVariableFunctionForLocality(automata, Locality.EXTERNAL_OUTPUT, HFileGenerator::generateVariableDeclaration, config, "Declare"))
+        // Internals
         result.append(Utils.performVariableFunctionForLocality(automata, Locality.INTERNAL, HFileGenerator::generateVariableDeclaration, config, "Declare"))
+        // Parameters
         result.append(Utils.performVariableFunctionForLocality(automata, Locality.PARAMETER, HFileGenerator::generateVariableDeclaration, config, "Declare"))
 
+        // Finally, if we have any delayed variables
         if(automata.variables.any({it.canBeDelayed()})) {
+            // Then we want to declare those too (these are only internal)
             result.appendln()
             result.appendln("${config.getIndent(1)}// Declare Delayed Variables")
 
-            for(variable in automata.variables
-                    .filter{it.canBeDelayed()}) {
+            // For each delayed variable
+            for(variable in automata.variables.filter{it.canBeDelayed()}) {
+                // Declare the delayed variant of the variable
                 result.appendln("${config.getIndent(1)}${Utils.createTypeName("Delayable", Utils.generateCType(variable.type))} ${Utils.createVariableName(variable.name, "delayed")};")
             }
         }
 
+        // Close the struct declaration with the type name (based on the automata name)
         result.appendln("} ${Utils.createTypeName(automata.name)};")
 
         return result.toString()
     }
 
+    /**
+     * Generates a string that represents a variable declaration
+     */
     private fun generateVariableDeclaration(variable: Variable): String {
+        // Simply just the type (converted to C type) and the variable name (conforming to style)
         return "${Utils.generateCType(variable.type)} ${Utils.createVariableName(variable.name)};"
     }
 }
